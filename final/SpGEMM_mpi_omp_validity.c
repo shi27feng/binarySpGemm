73828d41716293565468ea3a9e61f49ac156c961
feng <shi-feng@outlook.com>
2022-10-19 16:44:45 +0800

    formatting files

--- final/SpGEMM_mpi_omp_validity.c
+++ final/SpGEMM_mpi_omp_validity.c
@@ -152,47 +152,45 @@ void SpGEMM_mpi(int  *Acol, int *Arow, int An,
     int tasksize = An/numtasks; // number of rows an mpi task will have to compute
 
     int *Ccol_slice;    // Ccol is alocated inside SpGEMM_omp exactly
-    int *Crow_slice = malloc((tasksize+1)*sizeof(int));
+    int *Crow_slice = malloc((tasksize + 1) * sizeof(int));
 
     // call SpGEMM_omp only on the part each task has to do
-    SpGEMM_omp(Acol,&Arow[rank*tasksize],tasksize,
-                Bcol,Brow,Bm,
-                &Ccol_slice,Crow_slice,
-                tBlock);
+    SpGEMM_omp(Acol, &Arow[rank * tasksize], tasksize,
+               Bcol, Brow, Bm,
+               &Ccol_slice, Crow_slice,
+               tBlock);
     
     // find final nnz
-    int all_nnz=0;
-    MPI_Reduce(&Crow_slice[tasksize],&all_nnz,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-    
+    int all_nnz = 0;
+    MPI_Reduce(&Crow_slice[tasksize], &all_nnz, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     
     // find nnz per task needed for the Ccol gather
     int *reccounts;
     isroot{
-        reccounts = malloc(numtasks*sizeof(int));
+        reccounts = malloc(numtasks * sizeof(int));
     }
-    MPI_Gather(&Crow_slice[tasksize],1,MPI_INT,reccounts,1,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Gather(&Crow_slice[tasksize], 1, MPI_INT, reccounts, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     // compute displacement array needed for the Ccol gather on the root task
     int *disps;
     isroot{
-        disps = malloc(numtasks*sizeof(int));
+        disps = malloc(numtasks * sizeof(int));
         disps[0] = 0;
-        for(int t=1; t<numtasks; t++){
-            disps[t] = disps[t-1] + reccounts[t-1];
+        for(int t = 1; t < numtasks; t++){
+            disps[t] = disps[t - 1] + reccounts[t - 1];
         }
     }
 
     // gather results of each task needed for the Ccol gather on the root task
     isroot{
-        *Ccol = malloc(all_nnz*sizeof(int));
+        *Ccol = malloc(all_nnz * sizeof(int));
         Crow[0] = 0;
     }
-    MPI_Gatherv(Ccol_slice,Crow_slice[tasksize],MPI_INT,*Ccol,reccounts,disps,MPI_INT,0,MPI_COMM_WORLD);//gather Ccol
-    MPI_Gather(&Crow_slice[1],tasksize,MPI_INT,&Crow[1],tasksize,MPI_INT,0,MPI_COMM_WORLD);             //gather Crow
+    MPI_Gatherv(Ccol_slice, Crow_slice[tasksize], MPI_INT, *Ccol, reccounts, disps, MPI_INT, 0, MPI_COMM_WORLD); //gather Ccol
+    MPI_Gather(&Crow_slice[1], tasksize, MPI_INT, &Crow[1], tasksize, MPI_INT, 0, MPI_COMM_WORLD);                      //gather Crow
     
     free(Crow_slice);
     free(Ccol_slice);
-
 
     // fix Crow from representing each task to representing the whole matrix
     isroot{
@@ -200,52 +198,46 @@ void SpGEMM_mpi(int  *Acol, int *Arow, int An,
         free(reccounts);
 
         int row_sum = Crow[tasksize];
-        for(int i=1; i<numtasks; i++){
-            for(int j=1; j<tasksize+1; j++){
-                Crow[i*tasksize + j] += row_sum;
+        for(int i = 1; i < numtasks; i++){
+            for(int j = 1; j < tasksize + 1; j++){
+                Crow[i * tasksize + j] += row_sum;
             }
-            row_sum = Crow[(i+1)*tasksize];
+            row_sum = Crow[(i + 1) * tasksize];
         }
-        
     }
-
 }
 
 
-// serial implementation of masked SpGEMM
+// Serial implementation of masked SpGEMM
 // can easily be parallelized exactly as above
 // does C = F.*(A*B)
 // all in CSR form
 void SpGEMM_masked(int  *Acol, int *Arow, int An, 
                    int  *Bcol, int *Brow, int Bm,
                    int  *Fcol, int *Frow,
-                   int **Ccol, int *Crow, int *Csize)
+                   int **Ccol, int *Crow, int *Csize) {
+    int nnzcum = 0;
+    bool *xb = malloc(An * sizeof(bool));
+    for (int i = 0; i < An; i++) xb[i] = true;           // start at 1, then make false only the mask bits
 
-{
-
-    int nnzcum=0;
-    bool *xb = malloc(An*sizeof(bool));
-    for(int i=0; i<An; i++) xb[i] = true;           // start at 1, then make false only the mask bits
-
-    for(int i=0; i<An; i++){
+    for (int i = 0; i < An; i++) {
         int nnzpv = nnzcum; //nnz of previous row;
         Crow[i] = nnzcum;
-        if(nnzcum + An > *Csize){
-            *Csize += MAX(An, *Csize/4);
-            *Ccol = realloc(*Ccol,*Csize*sizeof(int));
+        if (nnzcum + An > *Csize) {
+            *Csize += MAX(An, *Csize / 4);
+            *Ccol = realloc(*Ccol, *Csize * sizeof(int));
         }
 
         //add row mask
-        for(int jj=Frow[i]; jj<Frow[i+1]; jj++){    // this and the part below using F is the only difference
+        for (int jj = Frow[i]; jj < Frow[i + 1]; jj++){    // this and the part below using F is the only difference
             xb[ Fcol[jj] ] = false;                 // to the normal algorithm.
         }                                           // It just turns the flag bits than can be added to 0
 
-
         // gustavson
-        for(int jj=Arow[i]; jj<Arow[i+1]; jj++){
+        for (int jj = Arow[i]; jj < Arow[i + 1]; jj++){
             int j = Acol[jj];
 
-            for(int kp=Brow[j]; kp<Brow[j+1]; kp++){
+            for (int kp = Brow[j]; kp < Brow[j + 1]; kp++){
                 int k = Bcol[kp];
                 if(!xb[k]){
                     xb[k] = true;
@@ -255,41 +247,33 @@ void SpGEMM_masked(int  *Acol, int *Arow, int An,
             }
 
         }
-        if(nnzcum > nnzpv){
-            quickSort(*Ccol,nnzpv,nnzcum-1);
-            for(int p=nnzpv; p<nnzcum; p++){
+        if (nnzcum > nnzpv){
+            quickSort(*Ccol, nnzpv, nnzcum - 1);
+            for(int p = nnzpv; p < nnzcum; p++){
                 xb[ (*Ccol)[p] ] = false;
             }
         }
-
-        
-        for(int jj=Frow[i]; jj<Frow[i+1]; jj++){   
+        for(int jj = Frow[i]; jj < Frow[i + 1]; jj++){   
             xb[ Fcol[jj] ] = true;             // reset the flag bits to 1
         }
-
     }
     Crow[An] = nnzcum;
-
     free(xb);
-
 }
 
 bool SpGEMM_valid(int *Acol, int *Arow, int *Bcol, int *Brow, int n){
-    for(int i=0; i<=n; i++){
-        if(Arow[i]!=Brow[i]){
+    for (int i = 0; i <= n; i++){
+        if (Arow[i] != Brow[i]){
             return false;
         }
     }
-    for(int i=0; i<Arow[n]; i++){
-        if(Acol[i]!=Bcol[i]){
+    for (int i = 0; i < Arow[n]; i++){
+        if(Acol[i] != Bcol[i]){
             return false;
         }
     }
     return true;
 }
-
-
-
 
 // test function
 int test_mpi(int argc, char const *argv[]){
@@ -298,7 +282,6 @@ int test_mpi(int argc, char const *argv[]){
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
     int tBlock  = atoi(argv[2]);
     int threads = atoi(argv[3]);
 
@@ -306,31 +289,27 @@ int test_mpi(int argc, char const *argv[]){
     
     uint32_t *Arow,*Acol;
     int An,Am,Annz;
-    readCOO(argv[1],&Arow,&Acol,&An,&Am,&Annz);
+    readCOO(argv[1], &Arow, &Acol, &An, &Am, &Annz);
 
-    int *nCrow = calloc((An+1),sizeof(int));
+    int *nCrow = calloc((An + 1), sizeof(int));
     int *nCcol;
-
 
     MPI_Barrier(MPI_COMM_WORLD);    // start the timer at the same time for all tasks
 
-
-    SpGEMM_mpi(Acol,Arow,An,Acol,Arow,An,&nCcol,nCrow,tBlock);
+    SpGEMM_mpi(Acol, Arow, An, Acol, Arow, An, &nCcol, nCrow, tBlock);
 
     isroot{
         int tCsize = nCrow[An];
-        int *tCrow = calloc((An+1),sizeof(int));
-        int *tCcol = malloc(tCsize*sizeof(int));
-        SpGEMM_bigslice(Acol,Arow,An,Acol,Arow,An,&tCcol,tCrow,&tCsize,0,An);
+        int *tCrow = calloc((An + 1), sizeof(int));
+        int *tCcol = malloc(tCsize * sizeof(int));
+        SpGEMM_bigslice(Acol, Arow, An, Acol, Arow, An, &tCcol, tCrow, &tCsize, 0, An);
 
-        if(SpGEMM_valid(nCcol,nCrow,tCcol,tCrow,An)){
+        if (SpGEMM_valid(nCcol, nCrow, tCcol, tCrow, An)){
             printf("Results of serial and multricore are the same!\n");
-        }else{
+        } else {
             printf("The results dont match\n");
         }
-
     }
-
 
     free(Acol);
     free(Arow);
@@ -339,25 +318,21 @@ int test_mpi(int argc, char const *argv[]){
     free(nCrow);
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int numtasks, rank, provided;
-    
 
     //initialize MPI
-    MPI_Init_thread(&argc,&argv,MPI_THREAD_FUNNELED,&provided);
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
     MPI_Query_thread(&provided);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if(argc!=4){
+    if(argc != 4){
         printf("usage: mpirun  -n  numtasks  SpGEMM_mpi_omp  path-to-matrix  threadslice_size  number_of_threads\n");
         exit(1);
     }
-    test_mpi(argc,argv);
     
-
+    test_mpi(argc, argv);
     MPI_Finalize();
     return 0;
 }
-
